4ec9426b45288c779efb65405ff17166f4284312
Honguito <eduardolalojami@gmail.com>
2021-08-31 12:17:41 -0500

    Reto 2 agrego interfaz con cupones

--- Taller-2/porcentajes.js
+++ Taller-2/porcentajes.js
@@ -22,6 +22,52 @@ function computePriceDiscount() {
     const ResultPrice = document.getElementById("ResultPrice");
     ResultPrice.innerText = "El precio con descuento es: $ " + precioConDescuento;
 }
+
+const coupons = [
+    {
+        name: "JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name: "pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name: "es_un_secreto",
+        discount: 25,
+    },
+];
+const combobox = document.getElementById("IDcoupons");
+let text = ``;
+for (let i = 0; i < coupons.length; i++) {
+    text += `<option value="`+coupons[i].discount+`">`+coupons[i].name+`</option>`;
+    combobox.innerHTML = text;
+}
+
+const isCouponValueValid = function (coupon) {
+    return coupon.name === couponValue;
+};
+
+const userCoupon = coupons.find(isCouponValueValid);
+
+if (!userCoupon) {
+    alert("El cupón " + userCouponValue + "no es válido");
+}
+if (!userCoupon) {
+    alert("El cupón " + userCouponValue + "no es válido");
+} else {
+    const descuento = userCoupon.discount;
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    const resultP = document.getElementById("ResultPrice");
+    ResultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
+
+const couponValue = combobox.value;
+
+function validCouponFunction() {
+    const userCoupon = coupons.find(isCouponValueValid);
+}
 // console.log({
 //     precioOriginal,
 //     descuento,
